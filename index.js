53a484f5768e14034fcfa7916511df994682ee21
gerrillari <tam.alcalajimenez@gmail.com>
2021-02-15 20:41:50 +0100

    2 files modified

--- index.js
+++ index.js
@@ -8,30 +8,57 @@ appDiv.innerHTML = `<h1>Le jeu du Shifumi</h1>`;
 document.getElementById("btn").addEventListener("click",(event)=>{//Lorsque la boutton est cliqué, ça va faire l'évenement d'à continuation
 var index1= document.getElementById('choix1').value;//Je récupère la valeur pour le j1
 var index2= document.getElementById('choix2').value;//Je récupère la valeur pour le j2
+console.log([index1, index2])//afficher les choix dans la console
 
-var result; // on crée variable result qui va prendre les résultats de la comparaison
-function compare (index1, index2){//On crée une fonction qui va lancer la comparaison des choix du j1 et du j2
-  if (index1==0 && index2==2){
-    result= "Joueur 2 a gagné"
+
+if (index1 === index2){ //faire le comparatif pour chaque choix
+  document.getElementById("resultat").innerText = "Egalité" //afficher les résultats pour chaque choix
+}else if (index1=="Pierre" && index2=="Papier"){
+     document.getElementById("resultat").innerText = "Joueur 2 a gagné"
   }
-  else if (index1==2 && index2==0){
-    result= "Joueur 1 a gagné"
+  else if (index1=="Papier" && index2=="Pierre"){
+    document.getElementById("resultat").innerText = "Joueur 1 a gagné"
   }
-  else if (index1==2 && index2==1){
-    result= "Joueur 2 a gagné"
+  else if (index1=="Papier" && index2=="Ciseaux"){
+    document.getElementById("resultat").innerText = "Joueur 2 a gagné"
   }
-  else if (index1==1 && index2==2){
-    result= "Joueur 1 a gagné"
+  else if (index1=="Ciseaux" && index2=="Papier"){
+    document.getElementById("resultat").innerText = "Joueur 1 a gagné"
   }
-  else if (index1==0 && index2==1){
-    result= "Joueur 1 a gagné"
+  else if (index1=="Pierre" && index2=="Ciseaux"){
+    document.getElementById("resultat").innerText = "Joueur 1 a gagné"
   }
-  else if (index1==1 && index2==0){
-    result= "Joueur 2 a gagné"
+  else if (index1=="Ciseaux" && index2=="Pierre"){
+    document.getElementById("resultat").innerText = "Joueur 2 a gagné"
   }
-  else {
-    result= "C'est égal"
-  }
-  }
-document.getElementById('resultat').value = result; //On affiche le résultat
+
 })
+
+//pour ne pas à avoir a copier coller le document.getElementById pour chaque choix: 
+//let result="" //on déclare variable
+//if (idem que dans le code)
+//mais changer le document.getEl.... par result="Egalité" et etc. //pour lui dire à quoi correspond
+//document.getElementById("resultat").innerText = result  pour afficher
+//let result = ""  
+//if (index1 === index2){ //faire le comparatif pour chaque choix
+//  result = "Egalité" //afficher les résultats pour chaque choix
+//}else if (index1=="Pierre" && index2=="Papier"){
+//     result = "Joueur 2 a gagné"
+//  }
+//  else if (index1=="Papier" && index2=="Pierre"){
+//    result = "Joueur 1 a gagné"
+//  }
+//  else if (index1=="Papier" && index2=="Ciseaux"){
+//    result = "Joueur 2 a gagné"
+//  }
+//  else if (index1=="Ciseaux" && index2=="Papier"){
+//    result = "Joueur 1 a gagné"
+//  }
+//  else if (index1=="Pierre" && index2=="Ciseaux"){
+//    result = "Joueur 1 a gagné"
+//  }
+//  else if (index1=="Ciseaux" && index2=="Pierre"){
+//    result = "Joueur 2 a gagné"
+//  }
+//result = result
+//})
